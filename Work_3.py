55d163880eec47d6e6527c3af0880c14c6f63813
YaroslavGavrilychev <gawrilichew@gmail.com>
2020-09-13 11:00:30 +0700

    Add Work_3.py

---
+++ Work_3.py
@@ -0,0 +1,22 @@
+months_list = ['Зима', 'Весна', 'Лето', 'Осень']
+months_dict = {1: 'Зима', 2: 'Весна', 3: 'Лето', 4: 'Осень'}
+month = int(input("Введите номер месяца: "))
+
+if month == 1 or month == 12 or month == 2:
+    print(months_list[0])
+    print(months_dict.get(1))
+
+elif month == 3 or month == 4 or month == 5:
+    print(months_list[1])
+    print(months_dict.get(2))
+
+elif month == 6 or month == 7 or month == 8:
+    print(months_list[2])
+    print(months_dict.get(3))
+
+elif month == 9 or month == 10 or month == 11:
+    print(months_list[3])
+    print(months_dict.get(4))
+
+else:
+    print("А в году всего 12 месяцев!")

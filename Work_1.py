5c8b1238e7fbcf2e26fc0fb51e6bffb12cf9d3b7
YaroslavGavrilychev <gawrilichew@gmail.com>
2020-09-13 10:58:33 +0700

    Add Work_1.py

---
+++ Work_1.py
@@ -0,0 +1,9 @@
+my_list = [32, 33.34, 'Yaroslav', True, (1, 2, 3), [4, 5, 6], {7, 8, 9}]
+
+print(type(my_list[0]))
+print(type(my_list[1]))
+print(type(my_list[2]))
+print(type(my_list[3]))
+print(type(my_list[4]))
+print(type(my_list[5]))
+print(type(my_list[6]))
